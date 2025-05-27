a600ef09faea919d73552cae156b340e88f3a437
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-27 20:45:38 -0300

    Criando entity

---
+++ generics/Entity.py
@@ -0,0 +1,23 @@
+from typing import Generic,TypeVar,Optional
+from abc import ABC
+from Identity import Identity
+
+T = TypeVar('T')
+
+class Entity(Generic[T],ABC):    
+    def __init__(self,properties:T,id:Optional[Identity]=None):
+        self.__id = id or Identity()
+        self.__properties = properties
+        
+    @property
+    def id(self)-> Identity:
+        return self.__id
+    
+    @property
+    def properties(self)-> T:
+        return self.__properties
+    
+    def __eq__(self, other):
+        if not isinstance(other, Entity):
+            return False
+        return self.id == other.id    
